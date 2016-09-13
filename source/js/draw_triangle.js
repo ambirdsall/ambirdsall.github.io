2e403a5c136bc8aa015e6a57edd4c587cea99ca2
ambirdsall <ambirdsall@gmail.com>
2016-09-13 14:18:50 -0700

    Automatically populate mini triangle in header

--- source/js/draw_triangle.js
+++ source/js/draw_triangle.js
@@ -6,63 +6,60 @@
 // which I adapted to my own purposes. The original lives (or lived, as of this
 // writing) at https://gist.github.com/tenderlove/5898231
 
-var w = 433*0.5,
-    h = 300*0.5,
-    padding = 10,
-    page = d3.select(window),
-    // Create SVG element in which to draw triangle
-    svg = d3.select("#triangle")
-            .insert("svg", ".name")
-            .attr("width", w)
-            .attr("height", h)
-            .classed("triangle", true),
+export default function triangleBuilder(scale, selector, color) {
+  var w = 433 * scale,
+      h = 300 * scale,
+      padding = 10,
+      page = d3.select(window),
+      // Create SVG element in which to draw triangle
+      svg = d3.select(selector)
+              .insert("svg", ".name")
+              .attr("width", w)
+              .attr("height", h)
+              .classed("triangle", true),
 
-    // d3 selections are nested arrays, as: [[node]]
-    scrollingContainer = d3.select("#scrollable")[0][0],
+      xScale = d3.scale.linear()
+                      .domain([0, w])
+                      .range([padding, w - padding]),
+      yScale = d3.scale.linear()
+                      .domain([0, w])
+                      .range([padding, w - padding]),
 
-    xScale = d3.scale.linear()
-                     .domain([0, w])
-                     .range([padding, w - padding]),
-    yScale = d3.scale.linear()
-                     .domain([0, w])
-                     .range([padding, w - padding]),
+      // start with the three vertices of the triangle
+      dataset = [[0, 0], [w, 0], [w / 2, h]],
+      triangle = dataset.slice(0),
 
-    // start with the three vertices of the triangle
-    dataset = [[0, 0], [w, 0], [w / 2, h]],
-    triangle = dataset.slice(0),
-
-    // Seed the triangle with a starting point in the middle
-    point = [w / 2, h / 2];
-dataset.push(point);
+      // Seed the triangle with a starting point in the middle
+      point = [w / 2, h / 2]
+  dataset.push(point)
 
 
-function draw(dataset) {
-  var circles = svg.selectAll("circle").data(dataset);
+  function draw(dataset) {
+    var circles = svg.selectAll("circle").data(dataset)
 
-  //Create circles
-  circles.enter()
-    .append("circle")
-    .attr("cx", function(d) { return xScale(d[0]); })
-    .attr("cy", function(d) { return yScale(d[1]); })
-    .attr("r", 1)
-    .style("fill", "#aa9668");
-}
-
-function genpoint(triangle, point) {
-  var vertex = triangle[Math.floor(Math.random() * triangle.length)];
-  var x = (vertex[0] + point[0]) / 2;
-  var y = (vertex[1] + point[1]) / 2;
-  return [x, y];
-}
-
-/*=============================*
- * Load up the event handlers! *
- *=============================*/
-export default function buildTriangle() {
-  // d3.event.preventDefault();
-  for (var i = 0; i < 8; ++i) {
-    point = genpoint(triangle, point);
-    dataset.push(point);
+    //Create circles
+    circles.enter()
+      .append("circle")
+      .attr("cx", function(d) { return xScale(d[0]); })
+      .attr("cy", function(d) { return yScale(d[1]); })
+      .attr("r", 1)
+      .style("fill", color)
   }
-  draw(dataset);
+
+  function genpoint(triangle, point) {
+    var vertex = triangle[Math.floor(Math.random() * triangle.length)]
+    var x = (vertex[0] + point[0]) / 2
+    var y = (vertex[1] + point[1]) / 2
+    return [x, y]
+  }
+
+  // triangle
+  // - 
+  return function buildGivenTriangle() {
+    for (var i = 0; i < 8; ++i) {
+      point = genpoint(triangle, point)
+      dataset.push(point)
+    }
+    draw(dataset)
+  }
 }
--- source/js/triangle_m.js
+++ source/js/triangle_m.js
@@ -1,10 +1,11 @@
-import buildTriangle from './draw_triangle.js'
-import { curry } from 'lodash-es'
+import triangleBuilder from './draw_triangle.js'
+import setIntervalX from './interval_n_times.js'
+// import { before } from 'lodash-es'
+
+var populateMainTriangle = triangleBuilder(0.5, '#triangle',  "#aa9668")
+var populateMiniTriangle = triangleBuilder(0.2, '#mini-triangle', '#224466')
 
 var page = d3.select(window)
-page.on("mousemove",    buildTriangle)
-page.on("devicemotion", buildTriangle)
-
-var addOne = curry(function add(a,b) { return a+b })(1)
-
-console.log(addOne(2))
+page.on("mousemove",    populateMainTriangle)
+page.on("devicemotion", populateMainTriangle)
+setIntervalX(populateMiniTriangle, 80, 100)
