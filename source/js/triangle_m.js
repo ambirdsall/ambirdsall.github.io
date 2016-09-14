02263628b24a426015968b203e94d069068d7d32
ambirdsall <ambirdsall@gmail.com>
2016-09-13 23:12:43 -0700

    populate mini triangle on page load; 0.3 scale, 20/200 timing

--- source/js/triangle_m.js
+++ source/js/triangle_m.js
@@ -1,11 +1,11 @@
 import triangleBuilder from './draw_triangle.js'
 import setIntervalX from './interval_n_times.js'
-// import { before } from 'lodash-es'
+// import { once } from 'lodash-es'
 
 var populateMainTriangle = triangleBuilder(0.5, '#triangle',  "#aa9668")
-var populateMiniTriangle = triangleBuilder(0.2, '#mini-triangle', '#224466')
+var populateMiniTriangle = triangleBuilder(0.3, '#mini-triangle', '#224466')
 
 var page = d3.select(window)
 page.on("mousemove",    populateMainTriangle)
 page.on("devicemotion", populateMainTriangle)
-setIntervalX(populateMiniTriangle, 80, 100)
+page.on('load', () => { setIntervalX(populateMiniTriangle, 20, 200) })
