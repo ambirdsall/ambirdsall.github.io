1b245470c5e557c703b9087001f9cf52fb73d1ff
Alex Birdsall <ambirdsall@gmail.com>
2019-11-24 22:05:34 -0800

    Add icons to the header

--- blag/src/components/header.js
+++ blag/src/components/header.js
@@ -1,11 +1,25 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import {
+  FaGithub,
+  FaLinkedin,
+  FaTwitter,
+} from "react-icons/fa"
+
 import "./header.css"
+
+const IconBar = _props => (
+  <div className="icon-bar">
+    <a href="https:www.github.com/ambirdsall"><FaGithub/></a>
+    <a href="https:www.linkedin.com/in/ambirdsall"><FaLinkedin/></a>
+    <a href="https:www.twitter.com/gobslapped"><FaTwitter/></a>
+  </div>
+)
 
 const Header = ({ pageTitle, withIcons }) => {
   const icons = withIcons
-        ? <h2>[icons go here]</h2>
+        ? <IconBar/>
         : <></>
 
   return <header>
@@ -16,7 +30,15 @@ const Header = ({ pageTitle, withIcons }) => {
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0, textAlign: 'center' }}>
+      <h1 style={{
+        margin: "1em auto",
+        textAlign: 'left',
+        fontWeight: 400,
+        fontSize: '3.14159em',
+        maxWidth: '70%',
+        height: '100%',
+        letterSpacing: '7px',
+      }}>
         <Link
           to="/"
           style={{
