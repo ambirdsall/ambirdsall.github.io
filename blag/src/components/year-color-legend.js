c6fbe4df306267a9bb19ea29ba8ae9713be3f424
Alex Birdsall <ambirdsall@gmail.com>
2019-12-02 22:01:19 -0800

    Auto-generate 5-year legend

--- blag/src/components/year-color-legend.js
+++ blag/src/components/year-color-legend.js
@@ -1,10 +1,13 @@
 import React from "react"
 import numberToColor from "../utils/number-to-color"
 
-const y = year => <span style={{color: numberToColor(year)}}>{year}</span>
+const y = year => <span style={{color: numberToColor(year)}} key={year}>{year}</span>
+const lastFiveYears = [0, 1, 2, 3, 4].map(n => new Date().getFullYear() - n)
+      .map(year => y(year))
+      .reduce((ys, y) => <>{ys} | {y}</>)
 
 export default function Legend() {
   return <p style={{textAlign: "center"}}>
-    ({y(2019)} | {y(2018)} | {y(2017)} | {y(2016)})
+    ({lastFiveYears})
   </p>
 }
