03a427dc2e6e3fe2824fbdf12addda161ae81787
Alex Birdsall <ambirdsall@gmail.com>
2019-12-22 23:02:06 -0800

    Run Prettier on index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,59 +13,64 @@ import "./index.css"
 const IndexPage = ({
   data: {
     allMarkdownRemark: { edges },
-    site: { siteMetadata: { title }},
+    site: {
+      siteMetadata: { title },
+    },
   },
 }) => {
   // Group the posts by topic, with each topic sorted by recency of post.
   // Is it inefficient to sort the entire topic each time a single post is added? Yes.
   // Do I care, given that the slow stuff happens at build time? No.
   const postsByTopic = edges
-        .filter(e => !e.node.frontmatter.draft)
-        .map(e => e.node)
-        .reduce((topics, n) => {
-          const { topic } = n.frontmatter
+    .filter(e => !e.node.frontmatter.draft)
+    .map(e => e.node)
+    .reduce((topics, n) => {
+      const { topic } = n.frontmatter
 
-          // nobody likes to evaluate `undefined.push(n)`
-          topics[topic] = topics[topic]
-            ? topics[topic]
-            : []
+      // nobody likes to evaluate `undefined.push(n)`
+      topics[topic] = topics[topic] ? topics[topic] : []
 
-          topics[topic].push(n)
-          topics[topic].sort((a, b) => newestFirst(a, b))
+      topics[topic].push(n)
+      topics[topic].sort((a, b) => newestFirst(a, b))
 
-          return topics
-        }, {})
+      return topics
+    }, {})
 
   // Order the topics by date of most recent post
   const Posts = Object.values(postsByTopic)
-        .sort((a, b) => newestFirst(a[0], b[0]))
-        .map(postList => {
-          const { topic } = postList[0].frontmatter
+    .sort((a, b) => newestFirst(a[0], b[0]))
+    .map(postList => {
+      const { topic } = postList[0].frontmatter
 
-          return <>
-                   <h3>{topic}</h3>
-                   <div>{
-                     postList.map(node => <PostLink
-                                            key={node.id}
-                                            post={node}
-                                          />)
-                   }</div>
-                 </>
-        })
+      return (
+        <>
+          <h3>{topic}</h3>
+          <div>
+            {postList.map(node => (
+              <PostLink key={node.id} post={node} />
+            ))}
+          </div>
+        </>
+      )
+    })
 
-  return <Layout defaultHeader={true}>
-    <SEO title="Home" />
-    <div style={{
-      margin: "auto",
-      width: "fit-content",
-    }}>
-      <Triangle/>
-      <h2 className="subtitle">html &lt; thoughts &gt; browser</h2>
-      <Legend/>
-      {Posts}
-    </div>
-  </Layout>
- }
+  return (
+    <Layout defaultHeader={true}>
+      <SEO title="Home" />
+      <div
+        style={{
+          margin: "auto",
+          width: "fit-content",
+        }}
+      >
+        <Triangle />
+        <h2 className="subtitle">html &lt; thoughts &gt; browser</h2>
+        <Legend />
+        {Posts}
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
 
