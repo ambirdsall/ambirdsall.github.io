9c59c3b24e614f0835e107fa21632a731e102b77
Alex Birdsall <ambirdsall@gmail.com>
2024-04-17 21:36:50 -0700

    Colorized years breadcrumb comments and reformatting

--- src/utils/number-to-color.js
+++ src/utils/number-to-color.js
@@ -1,3 +1,6 @@
+// TODO change something in the color besides its hue (saturation? lightness?) based on an
+// index, magnitude of `number`, or something that will distinguish between different
+// years with the same remainder
 export default function numberToColor(number) {
   // I tried a Clever Hash Function which output a six-digit hex number, but
   // adjacent years generated REALLY similar colors, which is a pretty
@@ -6,7 +9,7 @@ export default function numberToColor(number) {
   // length) ought to buy me enough time to find a couple more nice colors.
   //
   // Fun fact: I got this list by googling, verbatim, "nice hex colors".
-  const niceColors = ['#F8B195', '#F67280', '#C06C84', '#6C5B7B', '#355C7D']
+  const niceColors = ["#F8B195", "#F67280", "#C06C84", "#6C5B7B", "#355C7D"]
 
   return niceColors[number % niceColors.length]
 }
