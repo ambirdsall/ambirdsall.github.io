8a0e2f8475a2bb1e62d1ce4a05d97b0b1e7c85e9
Alex Birdsall <ambirdsall@gmail.com>
2024-05-08 11:55:01 -0700

    More robust datestamp parsing

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -3,8 +3,21 @@ import { Link } from "gatsby"
 
 import "./post-link.css"
 
-const year = date => new Date(date).getFullYear()
-const datestamp = date => new Date(date).toISOString().slice(0, 10)
+const datestamp = date => {
+  const { year, month, day } = new Intl.DateTimeFormat(undefined, {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  })
+    .formatToParts(new Date(date))
+    .filter(part => part.type !== "literal")
+    .reduce((acc, part) => {
+      acc[part.type] = part.value
+      return acc
+    }, {})
+
+  return `${year}-${month}-${day}`
+}
 
 const PostLink = ({
   post: {
