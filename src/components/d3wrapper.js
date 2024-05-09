f56d903f3a2824d130ae89cc2a38a7aa331bd907
Alex Birdsall <ambirdsall@gmail.com>
2024-05-09 12:09:22 -0700

    Stop cloning d3 shit every time hot modules get reloaded

--- src/components/d3wrapper.js
+++ src/components/d3wrapper.js
@@ -4,8 +4,18 @@ export default function D3Wrapper({ d3RenderFn, className }) {
   // d3 needs an actual DOM node to do its thing on, so let's give it one
   const el = useRef(null)
 
+  // Render the d3 visualization. If the d3 visualization is already in the DOM, though,
+  // do not recreate it; this avoids duplicated element shenanigans with hot module
+  // reloading during development.
   useEffect(() => {
-    d3RenderFn(el.current)
+    if (!window.d3Embeds) {
+      window.d3Embeds = new Map()
+    }
+
+    if (!window.d3Embeds.get(d3RenderFn)) {
+      d3RenderFn(el.current)
+      window.d3Embeds.set(d3RenderFn, true)
+    }
   }, [d3RenderFn])
 
   return <div className={className} ref={el} />
