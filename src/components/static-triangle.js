49d519dc550b0c826632ce85fa13fa6f9acf6890
Alex Birdsall <ambirdsall@gmail.com>
2024-05-09 13:25:38 -0700

    Clean up triangle vizes for blag post

--- src/components/static-triangle.js
+++ src/components/static-triangle.js
@@ -1,13 +1,15 @@
-import React, { useCallback } from "react"
+import React from "react"
 import D3Wrapper from "./d3wrapper"
 import StaticTriangle from "../visualizations/triangle/static-triangle"
 
 import "./triangle.css"
 
-export default function TriangleWrapper() {
-  const d3Triangle = useCallback(el => {
-    new StaticTriangle(el).run()
-  }, [])
-
-  return <D3Wrapper d3RenderFn={d3Triangle} className="triangle" />
+const d3Triangle = el => {
+  new StaticTriangle(el).run()
 }
+
+const TriangleWrapper = () => (
+  <D3Wrapper d3RenderFn={d3Triangle} className="triangle big-bottom" />
+)
+
+export default TriangleWrapper
--- src/components/vertices.js
+++ src/components/vertices.js
@@ -1,13 +1,15 @@
-import React, { useCallback } from "react"
+import React from "react"
 import D3Wrapper from "./d3wrapper"
 import Vertices from "../visualizations/triangle/just-the-vertices"
 
-export default function VerticesWrapper() {
-  const drawVertices = useCallback(el => {
-    const v = new Vertices(el)
-    v.update()
-    v.update()
-  }, [])
-
-  return <D3Wrapper d3RenderFn={drawVertices} className="triangle" />
+const drawVertices = el => {
+  const v = new Vertices(el)
+  v.update()
+  v.update()
 }
+
+const VerticesWrapper = () => (
+  <D3Wrapper d3RenderFn={drawVertices} className="triangle big-bottom" />
+)
+
+export default VerticesWrapper
