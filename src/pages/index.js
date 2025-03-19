f329b1d972b4cd4cf3e0d124d252c41a1317a728
Alex Birdsall <ambirdsall@gmail.com>
2025-03-19 13:29:19 -0700

    subtle self-promotion

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,35 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Nothing from "../components/nothing"
 import PostLink from "../components/post-link"
 import Triangle from "../components/triangle"
 import newestFirst from "../utils/newest-first"
 
 import "./index.css"
+
+const qp = new URLSearchParams(document.location.search)
+
+const IntroductoryHeader = () => {
+  let SubtleSelfPromotion = Nothing
+
+  if (qp.get("resume")) {
+    SubtleSelfPromotion = () => (
+      <aside>
+        (<a href="/resume">Here is my resumé</a>, if you're a software engineer
+        hiring sort)
+      </aside>
+    )
+  }
+
+  return (
+    <introduction>
+      <h1>hello, I'm Alex Birdsall</h1>
+      <p>I make music, cook food, and build websites and computer programs</p>
+      <SubtleSelfPromotion />
+    </introduction>
+  )
+}
 
 // Group the posts by topic, with each topic sorted by recency of post.
 const postsByTopic = postNodes => {
@@ -60,6 +84,7 @@ const IndexPage = ({
   return (
     <Layout>
       <Seo title="Home" />
+      <IntroductoryHeader />
       <Triangle />
       <h2 className="subtitle">html &lt; thoughts &gt; browser</h2>
       {Posts}
