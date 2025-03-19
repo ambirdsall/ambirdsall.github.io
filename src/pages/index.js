c304af372870acedaeb4d1566c530cc9b4de7223
Alex Birdsall <ambirdsall@gmail.com>
2025-03-19 13:26:43 -0700

    noop refactor: blog post {as,}sorting + whitespace

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,18 +9,9 @@ import newestFirst from "../utils/newest-first"
 
 import "./index.css"
 
-const IndexPage = ({
-  data: {
-    allMdx: { edges },
-    site: {
-      siteMetadata: { title },
-    },
-  },
-}) => {
-  // Group the posts by topic, with each topic sorted by recency of post.
-  // Is it inefficient to sort the entire topic each time a single post is added? Yes.
-  // Do I care, given that the slow stuff happens once, at build time? No.
-  const postsByTopic = edges
+// Group the posts by topic, with each topic sorted by recency of post.
+const postsByTopic = postNodes => {
+  const byTopic = postNodes
     .map(e => e.node)
     .reduce((topics, n) => {
       const { topic } = n.frontmatter
@@ -28,35 +19,44 @@ const IndexPage = ({
       // nobody likes to evaluate `undefined.push(n)`
       topics[topic] = topics[topic] || []
 
-      topics[topic].push(n)
+      topics[topic].push({ ...n, topic })
+      // Is it inefficient to sort the entire topic each time a single post is added? Yes.
+      // Do I care, given that the slow stuff happens once at build time? No.
       topics[topic].sort((a, b) => newestFirst(a, b))
 
       return topics
     }, {})
+  // Order the topic lists by date of most recent post
+  return Object.values(byTopic).sort((a, b) => newestFirst(a[0], b[0]))
+}
 
-  // Order the topics by date of most recent post
-  const Posts = Object.values(postsByTopic)
-    .sort((a, b) => newestFirst(a[0], b[0]))
-    .map(postList => {
-      const { topic } = postList[0].frontmatter
+const IndexPage = ({
+  data: {
+    allMdx: { edges: postNodes },
+    site: {
+      siteMetadata: { title },
+    },
+  },
+}) => {
+  const Posts = postsByTopic(postNodes).map(postList => {
+    const { topic } = postList[0].frontmatter
 
-      return (
-        <div key={topic}>
-          <h3>{topic}</h3>
-          <div>
-            {postList.map(node => (
-              <PostLink key={node.id} post={node} />
-            ))}
-          </div>
+    return (
+      <div key={topic}>
+        <h3>{topic}</h3>
+        <div>
+          {postList.map(node => (
+            <PostLink key={node.id} post={node} />
+          ))}
         </div>
-      )
-    })
+      </div>
+    )
+  })
 
-  // TODO: handle post volume in a sane way. There should be reasonable caps on category length and
-  // number on the root page; there should be a separate blag/ index route with a comprehensive
-  // list; and there should be a fuzzy search for specific posts (maybe just on posts index page).
-  // Just by title? Title and category? Title, category, AND FULL TEXT???? That may be flying too
-  // close to the sun.
+  // I'll figure out sane limits on the home page post listing after it goes insane, not
+  // before.
+  // TODO: There should be a separate blag/ index route with a comprehensive list and
+  // fuzzy search for specific posts (by title? category? full text content? who knows!).
   return (
     <Layout>
       <Seo title="Home" />
