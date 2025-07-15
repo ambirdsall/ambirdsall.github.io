f37f4efedd41f3918e77c1ea200703c080fcf308
Alex Birdsall <ambirdsall@gmail.com>
2025-07-15 10:34:44 -0700

    Delete .dir-locals.el
    It had been malformed, but after (not all that much) reflection, I
    realized it was both easier and smarter to globally support mdx in my
    emacs config than to fix a hack to shoehorn it in for just this project.

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-((markdown-mode
-  (eval . (when (and buffer-file-name
-                     (string-match "\\.mdx\\'" buffer-file-name))
-            (markdown-mode))))
- (nil . ((eval . (add-to-list 'auto-mode-alist '("\\.mdx\\'" . markdown-mode)))))
